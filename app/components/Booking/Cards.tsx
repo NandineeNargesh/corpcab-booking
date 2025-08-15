8eaf82074946483ea169a7a8e561d757993aec36
Nandinee Nargesh <157111132+NandineeNargesh@users.noreply.github.com>
2025-08-15 16:19:38 +0530

    Update Cards.tsx

--- app/components/Booking/Cards.tsx
+++ app/components/Booking/Cards.tsx
@@ -1,22 +1,31 @@
 import CardsList from 'data/CardsList';
 import React, { useState } from 'react';
+import Image from 'next/image';
 
 function Cards() {
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
 
   return (
-    <div className='p-4 pl-5 pr-5  '>
+    <div className='p-4 pl-5 pr-5'>
       <h2 className='text-[17px] font-semibold'>Payment Methods</h2>
-      <div className=' p-3    grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-5 grid-cols-5  mt-2 '>
+      <div className='p-3 grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-5 grid-cols-5 mt-2'>
         {CardsList.map((item, index) => (
           <div
-            key={item.id} // ✅ unique key
-            className={`transform transition-transform duration-200 p-1  w-[50px] border border-gray-300 cursor-pointer shadow-sm hover:shadow-lg hover:scale-115 hover:bg-blue-50 rounded-md flex justify-center ${
-              activeIndex === index ? 'bg-blue-100 transform transition-transform duration-200 scale-115 border-[2px] border-indigo-500' : ''
+            key={item.id}
+            className={`transform transition-transform duration-200 p-1 w-[50px] border border-gray-300 cursor-pointer shadow-sm hover:shadow-lg hover:scale-115 hover:bg-blue-50 rounded-md flex justify-center ${
+              activeIndex === index
+                ? 'bg-blue-100 transform transition-transform duration-200 scale-115 border-[2px] border-indigo-500'
+                : ''
             }`}
             onClick={() => setActiveIndex(index)}
           >
-            <img src={item.image} alt={item.name} width={40} height={50} />
+            <Image
+              src={item.image}
+              alt={item.name}
+              width={40}
+              height={50}
+              unoptimized={typeof item.image === 'string'} // avoid optimization for /public paths
+            />
           </div>
         ))}
       </div>
