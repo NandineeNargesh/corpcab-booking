1a33458c99ab08e832b0fea075673d1c258c620a
Nandinee Nargesh <157111132+NandineeNargesh@users.noreply.github.com>
2025-08-15 16:17:49 +0530

    Update CardsList.tsx

--- app/data/CardsList.tsx
+++ app/data/CardsList.tsx
@@ -1,33 +1,35 @@
-export default[
+// ✅ Import PNGs so Webpack processes them
+import vis from '../public/vis.png';
+import appl from '../public/appl.png';
+import g from '../public/g.png';
 
-{
-    id:1,
-    name:'Master Card',
-    image:'/master.svg'
-},
-
-{
-    id:2,
-    name:'Visa Card',
-    image:'/vis.png'
-},
-
-{
-    id:3,
-    name:'Apple Pay',
-    image:'/appl.png'
-},
-
-{
-    id:4,
-    name:'Phone Pay',
-    image:'/phone.svg'
-},
-{
-    id:5,
-    name:'Google Pay',
-    image:'/g.png'
-},
+export default [
+  {
+    id: 1,
+    name: 'Master Card',
+    image: '/master.svg' // SVG stays as direct path
+  },
+  {
+    id: 2,
+    name: 'Visa Card',
+    image: vis
+  },
+  {
+    id: 3,
+    name: 'Apple Pay',
+    image: appl
+  },
+  {
+    id: 4,
+    name: 'Phone Pay',
+    image: '/phone.svg'
+  },
+  {
+    id: 5,
+    name: 'Google Pay',
+    image: g
+  }
+];
 
 
 ]
